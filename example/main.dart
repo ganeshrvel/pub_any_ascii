330edfa9f32c4033a42daddd5deaa3badeec5484
Ganesh Rathinavel <ganeshrvel@outlook.com>
2026-06-11 03:05:56 +0530

    updated examples

--- example/main.dart
+++ example/main.dart
@@ -11,9 +11,13 @@ void main() {
   print(anyAscii('👑 🌴')); // :crown: :palm_tree:
   print(anyAscii('さいたま')); // saitama
   print(anyAscii('دمنهور')); // dmnhwr
+  print(anyAscii('Αθήνα')); // Athina
+  print(anyAscii('에 가 힣 널')); // E Ga Hih Neol
 
   print(anyAsciiChar('æ')); // ae
   print(anyAsciiChar('ß')); // ss
   print(anyAsciiChar('深')); // Shen
   print(anyAsciiChar('👑')); // :crown:
+  print(anyAsciiChar('ж')); // zh
+  print(anyAsciiChar('λ')); // l
 }
